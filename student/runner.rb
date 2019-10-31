30164e7756db97f1febba77d59712cc67aa81468
John Paolo <paolo716@gmail.com>
2019-10-31 09:58:41 +0800

    Project OOP and Dry

---
+++ student/runner.rb
@@ -0,0 +1,5 @@
+require_relative "student"
+
+test_student = Student.new("Jack", "Black")
+
+puts test_student.first_name
---
+++ student/student.rb
@@ -0,0 +1,20 @@
+class Student
+    attr_reader :first_name, :last_name
+    
+    def initialize(first, last)
+        @first_name = first
+        @last_name = last
+    end
+
+    # def first_name
+    #     @first_name
+    # end
+
+    # def last_name
+    #     @last_name
+    # end
+
+    def student_fullname
+        "#{first_name} #{last_name}"
+    end
+end
