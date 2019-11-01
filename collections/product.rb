084c3f30c8639066143343a130d5a3218eca9fdf
John Paolo <paolo716@gmail.com>
2019-11-01 22:34:09 +0800

    product completion

--- collections/product.rb
+++ collections/product.rb
@@ -1,4 +1,5 @@
 class Product
+    @@products = []
     attr_reader :id, :name, :quantity, :price
 
     def initialize(data={})
@@ -6,9 +7,23 @@ class Product
         @name = data[:name] || "Test Product"
         @quantity = data[:quantity] || 0
         @price = data[:price] || 0
+        @@products << self
     end
 
     def self.all
-        
+        @@products
+    end
+
+    def self.product_names 
+        #map
+        # @@products.map { |product| product.name}
+        @@products.map(&:name)
+    end
+
+    def self.products_to_order 
+        #select
+        #reject
+        # @@products.reject { |product| product.quantity > 0}
+        @@products.select { |product| product.quantity <= 0}
     end
 end
--- collections/product_spec.rb
+++ collections/product_spec.rb
@@ -2,7 +2,7 @@ require_relative "product"
 
 RSpec.describe Product do 
 
-    before do
+    before(:all) do
         @p1 = Product.new({id: 1, name: "Item1", quantity: 3, price: 25})
     end
 
@@ -31,5 +31,18 @@ RSpec.describe Product do
 
     it "returns the list of all product" do
         expect(Product.all).to eq([@p1])  
+    end
+
+    it "returns list of product names" do
+        expect(Product.product_names).to eq([@p1.name])
+    end
+
+    it "returns the list of sold out product" do
+        #create product, one with uantity 0 and another with quantity more than 0
+        p2 = Product.new({id: 2, name: "Item2", quantity: 5, price: 15})
+        p3 = Product.new({id: 3, name: "Item3", quantity: 0, price: 30})
+
+        #since one product quantity is 0 it has to be restocked
+        expect(Product.products_to_order).to eq([p3])
     end
 end
